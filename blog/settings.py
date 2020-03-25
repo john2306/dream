9ea62b0da20ff481f301760b5cd8c51d74cda041
john2306 <johnmendozauni@gmail.com>
2020-03-25 12:20:49 -0500

    email

--- blog/settings.py
+++ blog/settings.py
@@ -170,6 +170,14 @@ MAILCHIMP_API_KEY = ''
 MAILCHIMP_DATA_CENTER = ''
 MAILCHIMP_EMAIL_LIST_ID = ''
 
+#EMAIL_CONFIGURATION_FOR_SEND_EMAIL_WITH_hmatrix.global@gmail.com
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'hmatrix.global@gmail.com'
+EMAIL_HOST_PASSWORD = 'qazwsx,.-UNI2020'
+
 # Django Allauth
 
 AUTHENTICATION_BACKENDS = (
